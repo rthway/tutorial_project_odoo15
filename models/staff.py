e29a6d48d0fa5b81a9e6068ef2d35c1453a1f859
rthway <rthway@gmail.com>
2023-08-12 16:05:21 +0545

    _rec_name added which show recording name while we record data

--- models/staff.py
+++ models/staff.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 class RestStaff(models.Model):
     _name = 'rest.staff'
     _description = "This is the model for our staff"
+    _rec_name='name'
 
     name = fields.Char(string="Name", size=50)
     age = fields.Integer(string="Age")
