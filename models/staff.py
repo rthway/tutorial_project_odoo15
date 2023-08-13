f809ba74b13cb0120d22728739ab2caf52017816
rthway <rthway@gmail.com>
2023-08-13 21:04:43 +0545

    create one2many field line

--- models/staff.py
+++ models/staff.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields,api
 
 class RestStaff(models.Model):
     _name = 'rest.staff'
@@ -6,7 +6,7 @@ class RestStaff(models.Model):
     _rec_name='name'
     _order='age asc'
 
-    name = fields.Char(string="Name", size=50)
+    name = fields.Char(string="Name")
     age = fields.Integer(string="Age")
     dob = fields.Date(string="DOB")
     mobile = fields.Char(string="Mobile")
@@ -14,5 +14,19 @@ class RestStaff(models.Model):
     country_id = fields.Many2one('res.country', string='Country')
     country_ids = fields.Many2many('res.country', string='Countrys')
     country_code=fields.Char(string="Country Code", related="country_id.code")
+    staff_line_ids = fields.One2many('rest.staff.lines', 'connecting_field', string='Staff Line')
     
+    # @api.model
+    # def create(self, vals):
+    #     existing_staff = self.search([('name', '=', vals.get('name'))])
+    #     if existing_staff:
+    #         raise exceptions.ValidationError("A staff with this name already exists.")
+    #     return super(RestStaff, self).create(vals)
+    
+class RestStaffLines(models.Model):
+    _name = 'rest.staff.lines'
+    _description = "This is the model for staff lines"
 
+    connecting_field = fields.Many2one('rest.staff', string="Staff ID")
+    name = fields.Char(string="Name")
+    product_id = fields.Many2one('product.product', string="Product")
